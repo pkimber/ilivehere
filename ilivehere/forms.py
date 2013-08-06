94b50309a351d86bef8586c9206d95bfb11a9da6
Patrick Kimber <code@pkimber.net>
2013-08-06 17:52:54 +0100

    form styling

--- ilivehere/forms.py
+++ ilivehere/forms.py
@@ -4,6 +4,14 @@ from .models import Story
 
 
 class StoryForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(StoryForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'pure-input-1-2'})
+        self.fields['email'].widget.attrs.update({'class': 'pure-input-1-2'})
+        self.fields['title'].widget.attrs.update({'class': 'pure-input-2-3'})
+        self.fields['description'].widget.attrs.update({'class': 'pure-input-2-3'})
+
     class Meta:
         model = Story
         fields = ("name", "email", "area", "title", "description", )
