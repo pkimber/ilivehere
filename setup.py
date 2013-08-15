6c69645d5edcfe6074e75bb019d9277a22aba0b3
Patrick Kimber <code@pkimber.net>
2013-08-15 12:30:28 +0100

    version 0.0.03

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
             'templates/ilivehere/*.*',
         ],
     },
-    version='0.0.02',
+    version='0.0.03',
     description='ilivehere',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
