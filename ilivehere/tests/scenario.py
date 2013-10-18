8587a069e14d9b84790be9cc34b9b2918ef491f2
Patrick Kimber <code@pkimber.net>
2013-10-18 05:33:43 +0100

    Basic story create tests

--- ilivehere/tests/scenario.py
+++ ilivehere/tests/scenario.py
@@ -1,6 +1,11 @@
+from ilivehere.models import Area
 from ilivehere.tests.model_maker import make_area
 
 
 def default_scenario_ilivehere():
     make_area('Hatherleigh')
     make_area('Exbourne')
+
+
+def get_area_hatherleigh():
+    return Area.objects.get(name='Hatherleigh')
--- ilivehere/tests/test_story.py
+++ ilivehere/tests/test_story.py
@@ -1,15 +1,38 @@
-from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-from ilivehere.models import Story
 from ilivehere.tests.model_maker import (
-    make_area,
     make_story_anon,
     make_story_trust,
+)
+from ilivehere.tests.scenario import (
+    default_scenario_ilivehere,
+    get_area_hatherleigh,
+)
+from login.tests.scenario import (
+    default_scenario_login,
+    get_user_staff,
 )
 
 
 class TestStory(TestCase):
 
+    def setUp(self):
+        default_scenario_login()
+        default_scenario_ilivehere()
+
     def test_create_anon(self):
-        pass
+        make_story_anon(
+            name='Pat',
+            email='code@pkimber.net',
+            area=get_area_hatherleigh(),
+            title='Alpha Male',
+            description='completed their 300 mile paddle',
+        )
+
+    def test_create_trust(self):
+        make_story_trust(
+            user=get_user_staff(),
+            area=get_area_hatherleigh(),
+            title='10 Pub Barrel Pull Success',
+            description='10 Pub Pull on Saturday',
+        )
