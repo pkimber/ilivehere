f51d1a7e1f875494b9c4b1eb83ee39cdd0dc7428
Patrick Kimber <code@pkimber.net>
2013-10-26 16:28:18 +0100

    version 0.0.08

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
             'templates/story/*.*',
         ],
     },
-    version='0.0.07',
+    version='0.0.08',
     description='story',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
